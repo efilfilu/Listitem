55c6d4a23fdb1d95e8d310cac51fa9b210c8e541
Endalkachew <ebizuneh@gmail.com>
2019-10-25 22:27:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/listitem/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.listitem;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String [] Items;
+    String [] Price;
+    String []Description;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Resources res= getResources();
+        myListView=(ListView) findViewById(R.id.myListView);
+        Items=res.getStringArray(R.array.items);
+       // myListView.setAdapter(new ArrayAdapter<String>(this,R.layout.my_listview_details, Items));
+       Price=res.getStringArray(R.array.price);
+       Description=res.getStringArray(R.array.description);
+
+       // REFERENCING itemAdapter
+       itemAdapter itemAdapter = new itemAdapter(this, Items, Price, Description);
+       // now we have to use it
+        myListView.setAdapter(itemAdapter);
+//--------------------------------------------FOR IMAGE------
+       /* myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent showDetailActivity=new Intent(getApplicationContext(),detailActivity.class);
+                showDetailActivity.putExtra("key",position);
+                startActivity(showDetailActivity);
+
+        });*/
+
+    }
+}
---
+++ app/src/main/java/com/example/listitem/itemAdapter.java
@@ -0,0 +1,57 @@
+package com.example.listitem;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class itemAdapter extends BaseAdapter {
+    String []items;
+    String []price;
+    String []description;
+    LayoutInflater mInflater;
+    public itemAdapter(Context c, String[]a, String[]b, String []d){
+        this.items=a;
+        this.price=b;
+        this.description=d;
+        mInflater=(LayoutInflater)c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return items[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+            View v= mInflater.inflate(R.layout.main_list_detail,null);
+        TextView nameTextView=(TextView)v.findViewById(R.id.itemname);
+        TextView descriptionTextView=(TextView)v.findViewById(R.id.description);
+        TextView priceTextView = (TextView)v.findViewById(R.id.priceTextView);
+
+        String name=items[position];
+        String describ=description[position];
+        String prices=price[position];
+
+        nameTextView.setText(name);
+        descriptionTextView.setText(describ);
+        priceTextView.setText(prices);
+
+
+        return v;
+    }
+}
